6614fe9434713e4cc7001838362ef3f036d57441
rishindra07 <rishindra.k15@gmail.com>
2026-03-01 20:20:50 +0530

    addded-cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const notificationRoutes = require("./routes/notification.routes");
 const ruleRoutes = require("./routes/rule.routes");
@@ -7,8 +7,9 @@ const settingsRoutes = require("./routes/settings.routes");
 const healthRoutes = require("./routes/health.routes");
 const { authMiddleware } = require("./middleware/auth.middleware");
 
-const app = express();
 
+const app = express();
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
 app.use((req, res, next) => {
